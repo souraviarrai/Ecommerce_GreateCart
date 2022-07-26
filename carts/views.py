19b83da6f6128f8f4d28814f4b193a1f4b9b0c30
souravdungbung <lesouravdungbungchamlingrai@gmail.com>
2022-07-27 00:00:01 +0600

    till cart user login

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from carts.models import Cart, CartItem
 from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.http import HttpResponse
 def _cart_id(request):
@@ -109,8 +110,11 @@ def cart(request,total=0,quantity=0,cart_items=None):
     try:
         tax = 0
         grand_total = 0
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart = cart,is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user,is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart = cart,is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
@@ -130,6 +134,8 @@ def cart(request,total=0,quantity=0,cart_items=None):
 
 
     return render(request,'store/cart.html',context)
+
+@login_required(login_url='login')
 
 
 def checkout(request,total=0,quantity=0,cart_items=None):
